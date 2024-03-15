24738354f41fe0b633de93c10c6e8b6989048ae0
Samia <162901845+alsagithub@users.noreply.github.com>
2024-03-15 21:31:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// compare both side---
+var a = 5;
+var b = 5;
+console.log(a == b);
+//both side is not equal---
+var c = 7;
+var d = 9;
+console.log(c != d);
+//greater than--
+var e = 10;
+var f = 6;
+console.log(e > f);
+//less than ---
+var g = 8;
+var h = 20;
+console.log(g < h);
---
+++ app.ts
@@ -0,0 +1,23 @@
+// compare both side---
+let a:number=5;
+let b:number=5;
+console.log(a==b);
+//answer--true
+
+//both side is not equal---
+let c:number=7;
+let d:number=9;
+console.log(c!=d);
+//answer--true
+
+//greater than--
+let e:number=10
+let f:number=6
+console.log(e>f);
+//answer--true
+
+//less than ---
+let g:number=8;
+let h:number=20;
+console.log(g<h);
+//answer--true
